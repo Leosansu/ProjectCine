72d5718b643064fbe169eebaea81ea69810667ea
Leonam <leosuerte_@hotmail.com>
2024-09-06 10:19:31 -0300

    Iniciando projeto cinema.

---
+++ ProjetoCineApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCineApplication.class, args);
+	}
+
+}
